e80de24b0b798ac235ba09529e17eeee224baf3a
santoshhalke <halkesantosh1577@gmail.com>
2025-03-26 14:11:22 +0530

    added the database functionality

--- src/main/java/com/university/Student.java
+++ src/main/java/com/university/Student.java
@@ -1,11 +1,14 @@
 package main.java.com.university;
-// import java.lang.*;
+import java.sql.*;
+import java.util.*;
 
 public class Student extends AcademicPerson implements Printable{
     int studentID;
     String major;
     private double gpa;
     static int totalStudents = 0;
+    private String url = "jdbc:mysql://localhost:3306/jspm";
+    private String username = "root", password = "1577";
 
     Student(String name, int studentID, String major){
         this.name = name;
@@ -14,15 +17,52 @@ public class Student extends AcademicPerson implements Printable{
         totalStudents++;
 
         // updating the database
+        try{
+            // make connection with drivers
+            Connection connection = DriverManager.getConnection(url, username, password);
 
+            // prepare statment
+            String query = "INSERT INTO Students(Name, Major, StudentID) VALUES(?, ?, ?);";
+
+            // Preapared statement
+            PreparedStatement ptst = connection.prepareStatement(query);
+            ptst.setString(1, this.name);
+            ptst.setString(2, this.major);
+            ptst.setInt(3, this.studentID);
+            int result = ptst.executeUpdate();
+            System.out.println("Updated the student table..");
+        }catch(SQLException s){
+            System.out.println("SQL exception : "+ s.getStackTrace());
+        }
     }
 
     static int getTotalStudents(){
         return totalStudents;
     }
 
-    void enrollInCourse(String courseName){
+    void enrollInCourse(String courseName, int courseID, int semster, int year){
+
+        try{
+            // build the connection
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // prepare for the query
+            String query = "INSERT INTO Enrollments(StudentID,CourseID,Semester,Year) VALUES(?, ?, ?, ?);";
+
+            // set prepared statement
+            PreparedStatement ptst = connection.prepareStatement(query);
+            ptst.setInt(1, this.studentID);
+            ptst.setInt(2, courseID);
+            ptst.setInt(3, semster);
+            ptst.setInt(4, year);
+            int result = ptst.executeUpdate();
+            System.out.println("Updated the enrollments table..");
+        }catch(SQLException s){
+            System.out.println("SQL exception ; " + s.getMessage());
+        }
+
         System.out.println(name + " enrolled in " + courseName);
+
     }
 
     public void setGpa(double gpa){
@@ -30,6 +70,21 @@ public class Student extends AcademicPerson implements Printable{
             throw new IllegalArgumentException("Invalid GPA");
         }else{
             this.gpa = gpa;
+
+            try{
+                Connection connection = DriverManager.getConnection(url, username, password);
+
+                String query = "UPDATE Students SET GPA=? WHERE studentID=?;";
+
+                PreparedStatement ptst = connection.prepareStatement(query);
+                ptst.setDouble(1, this.gpa);
+                ptst.setInt(2, this.studentID);
+                int result = ptst.executeUpdate();
+                System.out.println("GPA updated successfully..");
+
+            }catch(SQLException s){
+                System.out.println("SQL exception ; " + s.getMessage());
+            }
         }
     }
 
--- src/main/java/com/university/UniversityApp.java
+++ src/main/java/com/university/UniversityApp.java
@@ -2,22 +2,20 @@ package main.java.com.university;
 
 public class UniversityApp {
     public static void main(String[] args) {
-        University university = new University(6020, "SPPU");
-        // University university = new University();
-
-//        Student stud1 = new Student("Santosh", 10, "Information Technology");
+//        University university = new University(6020, "SPPU");
+        Student stud1 = new Student("Omkar", 72, "Information Technology");
 //        Student stud2 = new Student("Vishal", 52, "Mechanical Engineering");
 
 //        university.addStudent(stud1);
 //        university.addStudent(stud2);
 //
-//        stud1.enrollInCourse("Data Structure and Algorithm");
+//        stud1.enrollInCourse("Data Structure and Algorithm",1, 2, 3);
 //
-//        try {
-//            stud1.setGpa(3.2);
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
+        try {
+            stud1.setGpa(9.8);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 //
 //        System.out.println("Student GPA: " + stud1.getGpa());
 //        System.out.println("Total Students: " + Student.getTotalStudents());
