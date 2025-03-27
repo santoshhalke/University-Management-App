3c7aecdd6cf73c47d0792b76c77e485e0fed5305
santoshhalke <halkesantosh1577@gmail.com>
2025-03-27 13:55:20 +0530

    connected to the database

--- src/main/java/com/university/Professor.java
+++ src/main/java/com/university/Professor.java
@@ -1,11 +1,31 @@
 package main.java.com.university;
+import java.sql.*;
 
 public class Professor extends AcademicPerson{
     String department;
+    private String url = "jdbc:mysql://localhost:3306/jspm";
+    private String username = "root", password = "1577";
 
     Professor(String name, String department){
         this.name = name;
         this.department = department;
+
+        try{
+            // create a connection
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // query
+            String query = "INSERT INTO Professors(Name, Department) VALUES(?, ?);";
+
+            // Prepare statement
+            PreparedStatement ptst = connection.prepareStatement(query);
+            ptst.setString(1, this.name);
+            ptst.setString(2, this.department);
+            int result = ptst.executeUpdate();
+            System.out.println("Update the professor table..");
+        }catch(SQLException s){
+            System.out.println("SQL Exception : " + s.getMessage());
+        }
     }
 
     @Override
--- src/main/java/com/university/UniversityApp.java
+++ src/main/java/com/university/UniversityApp.java
@@ -3,7 +3,7 @@ package main.java.com.university;
 public class UniversityApp {
     public static void main(String[] args) {
 //        University university = new University(6020, "SPPU");
-        Student stud1 = new Student("Omkar", 72, "Information Technology");
+//        Student stud1 = new Student("Omkar", 72, "Information Technology");
 //        Student stud2 = new Student("Vishal", 52, "Mechanical Engineering");
 
 //        university.addStudent(stud1);
@@ -11,11 +11,11 @@ public class UniversityApp {
 //
 //        stud1.enrollInCourse("Data Structure and Algorithm",1, 2, 3);
 //
-        try {
-            stud1.setGpa(9.8);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+//        try {
+//            stud1.setGpa(9.8);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
 //
 //        System.out.println("Student GPA: " + stud1.getGpa());
 //        System.out.println("Total Students: " + Student.getTotalStudents());
